cc106e422bf82b9574486a00cf6a3c9129a0c3e4
UnrealEscapeBuilding <Hassom4142@Gmail.com>
2019-03-11 19:47:51 +0300

    إظهار نص تحذير إذا كان البلاير كونترولر موجود في الدبابة

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -1,7 +1,21 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include "BattleTank.h"
 #include "TankPlayerController.h"
 
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank) {
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController Not Possesenig a Tank"));
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController Possesenig : %s"), *(ControlledTank->GetName()));
+	}
+}
 
 ATank * ATankPlayerController::GetControlledTank() const
 {
--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -18,6 +18,7 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 public:
 	ATank * GetControlledTank() const;
 
+	virtual void BeginPlay() override;
 	
 	
 	
