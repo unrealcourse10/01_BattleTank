3c6e534978970caf59a29d1fdae1786f42ffa215
UnrealEscapeBuilding <Hassom4142@Gmail.com>
2019-05-15 15:12:41 +0300

    find the crosshair position in pixel coordinates

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -39,7 +39,7 @@ void ATankPlayerController::AimTowardsCrosshair()
 
 	if(GetSightRayHitLocation(OutHitLocation)) //Has a "Side-Effect", is going to line trace
 	{ 
-	UE_LOG(LogTemp, Warning, TEXT("HitLocation : %s"), *OutHitLocation.ToString());
+	//UE_LOG(LogTemp, Warning, TEXT("HitLocation : %s"), *OutHitLocation.ToString());
 
 		//TODO tell controlled tank to aim at this point
 	}
@@ -48,6 +48,12 @@ void ATankPlayerController::AimTowardsCrosshair()
 //get world location of linetrace through crosshair, true if hits landscape
 bool ATankPlayerController::GetSightRayHitLocation(FVector& OutHitlocation) const
 {
-	OutHitlocation = FVector(1.0);
+	//find the crosshair position in pixel coordinates
+	int32 ViewportSizeX, ViewportSizeY;
+	GetViewportSize(ViewportSizeX, ViewportSizeY);
+	auto ScreenLocation = FVector2D(ViewportSizeX * CrosshairXLocation, ViewportSizeY * CrosshairYLocation);
+	////UE_LOG(LogTemp, Warning, TEXT("ScreenLocation : %s"), *ScreenLocation.ToString());
+	//"De-project" the screen position of the crosshair to a world direction
+	//line-trace along that look direction, and see what we hit (up to max rage)
 	return true;
 }
--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -15,12 +15,14 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
+
 public:
+virtual void BeginPlay() override;
+virtual void Tick(float DeltaTime) override;
+
+private:
 	ATank * GetControlledTank() const;
 
-	virtual void BeginPlay() override;
-	
-	virtual void Tick( float DeltaTime ) override;
 
 	//Start the Tank moving the barrel so that a shot would hit where the crosshair intersects the world
 	//privet
@@ -29,5 +31,11 @@ public:
 	//privet
 	//return an out prameter, true if hit landscape 
 	bool GetSightRayHitLocation(FVector& OutHitlocation) const;
+
+	UPROPERTY(EditAnyWhere)
+		float CrosshairXLocation = 0.5;
+
+	UPROPERTY(EditAnyWhere)
+		float CrosshairYLocation = 0.3333;
 	
 };
